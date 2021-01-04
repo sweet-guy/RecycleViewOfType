51d165e1972c12d9563d01392564044d3a185d7f
yanxin <397667137@qq.com>
2021-01-04 10:24:40 +0800

    添加agentwebview，可输入在线文档链接查看效果

--- app/src/main/java/com/wdcloud/recycleviewoftype/OnlineWebView.java
+++ app/src/main/java/com/wdcloud/recycleviewoftype/OnlineWebView.java
@@ -5,27 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+
+import com.just.agentweb.AgentWeb;
+import com.just.agentweb.NestedScrollAgentWebView;
 
 public class OnlineWebView extends AppCompatActivity {
-    //在线文档链接
-    private String realUrl=".................";
+    //TODO :注意world后缀大写才可以加载
+    //使用在线文档链接
+    private String realUrl="xxxx.DOC";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_online_web_view);
-        WebView webView =findViewById(R.id.web);
-        String finalUrl = "http://view.officeapps.live.com/op/view.aspx?src=" + realUrl + "";
-        webView.loadUrl(finalUrl);
-        WebSettings settings = webView.getSettings();
-        settings.setJavaScriptEnabled(true);
-        settings.setUseWideViewPort(true);
-        settings.setAllowFileAccessFromFileURLs(true);
-        webView.setWebViewClient(new WebViewClient() {
-            @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url) {
-                view.loadUrl(url);
-                return true;
-            }
-        });
+        LinearLayout webView =findViewById(R.id.agent_parent);
+        NestedScrollAgentWebView scrollAgentWebView= new NestedScrollAgentWebView(this);
+        AgentWeb.with(this)
+                .setAgentWebParent(webView, new LinearLayout.LayoutParams(-1, -1))//传入AgentWeb 的父控件 ，如果父控件为 RelativeLayout ， 那么第二参数需要传入 RelativeLayout.LayoutParams
+                .useDefaultIndicator()
+                .setWebView(scrollAgentWebView)// 使用默认进度条
+                .createAgentWeb()//
+                .ready()
+                .go("http://view.officeapps.live.com/op/view.aspx?src="+realUrl);
     }
 }
